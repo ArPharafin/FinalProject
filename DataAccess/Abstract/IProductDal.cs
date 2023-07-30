ea9c4114721ff95622491f7c2c38228e2de9a21a
Ömer Faruk YILMAZ <109068669+ArPharafin@users.noreply.github.com>
2023-07-31 00:35:49 +0300

    31.07.23

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -5,14 +5,12 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal
-    {
-        List<Product> GetAll();
-        void Add(Product product);
-        void Update(Product product);
-        void Delete(Product product);
-
-        List<Product> GetAllByCategory(int categoryID);
-    
+    public interface IProductDal : IEntityRepository<Product> {
     }
-}  
+
+}
+
+
+
+
+    
